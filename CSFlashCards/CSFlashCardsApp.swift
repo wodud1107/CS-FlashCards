884c7c09818274ec9cff1fe09588a6f9ec300690
wodud1107 <wodud7992@naver.com>
2025-07-08 14:10:18 +0900

    Initial Commit

---
+++ CSFlashCards/CSFlashCardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CSFlashCardsApp.swift
+//  CSFlashCards
+//
+//  Created by 김재영 on 7/8/25.
+//
+
+import SwiftUI
+
+@main
+struct CSFlashCardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CSFlashCardsTests/CSFlashCardsTests.swift
@@ -0,0 +1,17 @@
+//
+//  CSFlashCardsTests.swift
+//  CSFlashCardsTests
+//
+//  Created by 김재영 on 7/8/25.
+//
+
+import Testing
+@testable import CSFlashCards
+
+struct CSFlashCardsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
